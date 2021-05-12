dd270731efa2f666bf045fb54d19e608f9831f0c
dama anusha <damaanusha9310@gmail.com>
2021-05-12 10:41:08 +0530

    ArraySums

---
+++ ArraySums/src/practise/twentyeight.java
@@ -0,0 +1,21 @@
+package practise;
+
+
+	import java.util.*;
+	public class twentyeight {
+
+	  public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    System.out.print("Enter number of elements ");
+	    int n = sc.nextInt();
+	    int[] a =new int[n];
+	    int i,j;
+	    for(i=0;i<n;i++) 
+	    {
+	      a[i]=sc.nextInt();
+	    }
+	  }
+
+	
+
+}
